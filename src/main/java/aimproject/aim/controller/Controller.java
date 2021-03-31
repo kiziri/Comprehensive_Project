89ad27aa483a9ea6e5547735bb000f0193c79f8d
kiziri <58429073+kiziri@users.noreply.github.com>
2021-03-31 19:28:45 +0900

    Add Controller class file for MVC controll

---
+++ src/main/java/aimproject/aim/controller/Controller.java
@@ -0,0 +1,13 @@
+package aimproject.aim.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @RequestMapping("/home")
+    public String home() {
+        return "page/main_page";
+    }
+}
