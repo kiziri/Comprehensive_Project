74a12a098e981b72c02720ed53e595b04b31af29
kiziri <58429073+kiziri@users.noreply.github.com>
2021-04-09 23:26:54 +0900

    Edit Controller file for add login control edit login_layout file for setup login_page

--- src/main/java/aimproject/aim/controller/Controller.java
+++ src/main/java/aimproject/aim/controller/Controller.java
@@ -1,13 +1,20 @@
 package aimproject.aim.controller;
 
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
     @RequestMapping("/")
-    public String home() {
+    public String home(Model model) {
         return "page/main_page";
     }
+
+    @RequestMapping("/login")
+    public String login(Model model) {
+        return "page/login_page";
+    }
+
 }
