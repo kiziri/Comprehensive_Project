cfa071c4ef8bf7c47a75fd0d12b1a508bdb2a6e3
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-08 00:36:16 +0900

    Edit Member join function controller

--- src/main/java/aimproject/aim/controller/Membercontroller.java
+++ src/main/java/aimproject/aim/controller/Membercontroller.java
@@ -1,2 +1,40 @@
-package aimproject.aim.controller;public class Membercontroller {
+package aimproject.aim.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class Membercontroller {
+
+    
+
+    @GetMapping("/join")
+    public String join(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+
+        return "page/join_page";
+    }
+
+    @PostMapping("/join")
+    public String join(@Valid MemberForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "page/login_page";
+    }
+
 }
