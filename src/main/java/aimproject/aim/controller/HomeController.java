2f6434ce9567420c2f0b811e7ce99a24ac87da14
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-08 00:36:56 +0900

    Edit controller to move member controller

--- src/main/java/aimproject/aim/controller/HomeController.java
+++ src/main/java/aimproject/aim/controller/HomeController.java
@@ -1,24 +1,15 @@
 package aimproject.aim.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@org.springframework.stereotype.Controller
-public class Controller {
+@Controller
+public class HomeController {
 
     @RequestMapping("/")
     public String home(Model model) {
         return "page/main_page";
-    }
-
-    @RequestMapping("/login")
-    public String login(Model model) {
-        return "page/login_page";
-    }
-
-    @RequestMapping("/join")
-    public String join(Model model) {
-        return "page/join_page";
     }
 }
