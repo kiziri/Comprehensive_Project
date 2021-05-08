d970dafcba0980abb6b354027a01e5353bf787a5
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-09 02:39:26 +0900

    Edit file code for analysisHistory entity

--- src/main/java/aimproject/aim/model/AnalysisHistory.java
+++ src/main/java/aimproject/aim/model/AnalysisHistory.java
@@ -1,4 +1,31 @@
 package aimproject.aim.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Getter @Setter
 public class AnalysisHistory {
+
+    @Id @GeneratedValue
+    @Column(name = "image_id")
+    private Long id;
+
+
+    private String imageName;
+
+
+    private String imagePath;
+
+
+    private Date date;
+
+
+    private String memberId;
 }
