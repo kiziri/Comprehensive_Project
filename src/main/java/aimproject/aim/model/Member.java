763b860376875de4b67d11d949eda277dfff7dd1
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-09 02:40:06 +0900

    Edit file code for member entity

--- src/main/java/aimproject/aim/model/Member.java
+++ src/main/java/aimproject/aim/model/Member.java
@@ -1,4 +1,38 @@
 package aimproject.aim.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
 public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "member_id")
+    private String memberId;
+
+    @Column(name = "member_pw")
+    private String memberPw;
+
+    @Column(name = "member_name")
+    private String name;
+
+    @Column(name = "member_nickname")
+    private String nickName;
+
+    @Column(name = "member_telnumber")
+    private String telNumber;
+
+    @Column(name = "member_address")
+    private String address;
+
+    @OneToMany(mappedBy = "member")
+    private List<AnalysisHistory> histories = new ArrayList<>();
 }
