609bcf697fcf5bb8bbaa7c72e4dc1e289cc996d6
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-09 02:39:55 +0900

    Edit file code for image entity

--- src/main/java/aimproject/aim/model/Image.java
+++ src/main/java/aimproject/aim/model/Image.java
@@ -1,4 +1,27 @@
 package aimproject.aim.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
 public class Image {
+
+    @Id @GeneratedValue
+    @Column(name = "history_id")
+    private Long id;
+
+    private String imageName;
+
+    private Long happyResult;
+    private Long sadResult;
+    private Long neutralResult;
+    private Long angryResult;
+    private Long fearResult;
+    private Long surpriseResult;
 }
