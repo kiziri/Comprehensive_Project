fa6b02ce2f8418361247d0af143fd29d28c87780
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-11 02:34:57 +0900

    Edit file for duplicate join and login handler from single controller

--- src/main/java/aimproject/aim/controller/MemberController.java
+++ src/main/java/aimproject/aim/controller/MemberController.java
@@ -47,37 +47,4 @@ public class MemberController {
         return "redirect:/";
     }
 
-    @RequestMapping("/login")
-    public String login(Model model) {
-        model.addAttribute("memberForm", new MemberForm());
-
-        return "page/login_page";
-    }
-
-    @GetMapping("/members/login")
-    public String login(LoginForm form, @RequestParam(value = "memberEmail") String memberEmail,
-                        @RequestParam(value = "memberPw") String memberPw, HttpServletRequest request) {
-
-        if(memberService.LoginMember(memberEmail,memberPw)){
-            //회원가입 결과가 참이라면
-            HttpSession session = request.getSession();
-            session.setAttribute("loginCheck",true);//세션을 생성
-            session.setAttribute("memberEmail", memberEmail);//세션의 아이디를 저장, 혹은 아이디에 맞는 닉네임도 같이 저장
-            return "redirect:/";//메인으로 이동
-        }
-        else{
-
-        }
-        return "page/login_page";//실패시 다시 로그인페이지로 이동
-    }
-
-    @GetMapping("/logout")
-    public String logout(HttpServletRequest request) {
-
-        //서비스 멤버 삭제
-        HttpSession session = request.getSession();
-        session.invalidate();//세션 삭제
-        return "redirect:/";//메인으로 이동
-    }
-
 }
