aa7d1e651af17c47c74e8402ab12b959ff5dd5f5
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-10 09:50:42 +0900

    Add join repository method

--- src/main/java/aimproject/aim/repository/MemberRepository.java
+++ src/main/java/aimproject/aim/repository/MemberRepository.java
@@ -1,10 +1,20 @@
 package aimproject.aim.repository;
 
+import aimproject.aim.model.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
 
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void join(Member member) {       // 회원가입
+        em.persist(member);
+    }
+
 
 }
