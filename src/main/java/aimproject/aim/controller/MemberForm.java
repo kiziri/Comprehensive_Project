5eb61142d50d4ec94ea39db31a27de02891daf63
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-10 13:40:58 +0900

    Edir MemberForm to get form data from front

--- src/main/java/aimproject/aim/controller/MemberForm.java
+++ src/main/java/aimproject/aim/controller/MemberForm.java
@@ -3,6 +3,25 @@ package aimproject.aim.controller;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter @Setter
 public class MemberForm {
+
+    @NotEmpty(message = "회원 아이디는 필수 입니다.")
+    private String memberId;
+
+    @NotEmpty(message = "회원 비밀번호는 필수 입니다.")
+    private String memberPw;
+    
+    @NotEmpty(message = "회원 이름은 필수 입니다.")
+    private String name;
+
+    @NotEmpty(message = "회원 닉네임은 필수 입니다.")
+    private String nickName;
+
+    @NotEmpty(message = "회원 전화번호는 필수 입니다.")
+    private String telNumber;
+    
+    private String address;
 }
