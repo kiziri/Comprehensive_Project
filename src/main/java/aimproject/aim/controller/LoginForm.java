9bc19574630ec12f58976fefc991655b96cd8330
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-11 00:26:49 +0900

    Add LoginForm to get login form data

---
+++ src/main/java/aimproject/aim/controller/LoginForm.java
@@ -0,0 +1,17 @@
+package aimproject.aim.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class LoginForm {
+
+    @NotEmpty(message = "회원 이메일는 필수 입니다.")
+    private String Email;
+
+    @NotEmpty(message = "회원 비밀번호는 필수 입니다.")
+    private String password;
+}
