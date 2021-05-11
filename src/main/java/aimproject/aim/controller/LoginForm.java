815d039c1bb0a53909cf85b30117e0ff4c2698e6
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-11 11:27:05 +0900

    Edit LoginForm object form input update LoginForm typo

--- src/main/java/aimproject/aim/controller/LoginForm.java
+++ src/main/java/aimproject/aim/controller/LoginForm.java
@@ -12,7 +12,7 @@ public class LoginForm {
     private Long memberId;
 
     @NotEmpty(message = "회원 이메일는 필수 입니다.")
-    private String Email;
+    private String email;
 
     @NotEmpty(message = "회원 비밀번호는 필수 입니다.")
     private String password;
