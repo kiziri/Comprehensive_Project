fa043453f85ad16a51276e9c770907bdde7682ae
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-18 03:08:12 +0900

    Update image service function file

--- src/main/java/aimproject/aim/service/ImageService.java
+++ src/main/java/aimproject/aim/service/ImageService.java
@@ -1,2 +1,16 @@
-package aimproject.aim.service;public class ImageService {
+package aimproject.aim.service;
+
+import aimproject.aim.repository.ImageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ImageService {
+
+    private ImageRepository imageRepository;
+
+
 }
