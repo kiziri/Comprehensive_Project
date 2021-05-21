e01a52813ac4b06246c9bdf875cdf37ed5ceef4c
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-22 01:54:28 +0900

    Update image service file

--- src/main/java/aimproject/aim/service/ImageService.java
+++ src/main/java/aimproject/aim/service/ImageService.java
@@ -1,5 +1,6 @@
 package aimproject.aim.service;
 
+import aimproject.aim.model.Image;
 import aimproject.aim.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,37 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ImageService {
 
-    private ImageRepository imageRepository;
+    private final ImageRepository imageRepository;
+
+    /**
+     * 이미지 정보 저장
+     */
+    @Transactional
+    public void save(Image image) {
+        imageRepository.save(image);
+    }
+
+    /**
+     * 이미지 전체 조회
+     */
 
 
+    /**
+     * 이미지 건별 조회
+     */
+
+
+    /**
+     * 이미지 아이디와 단건 조회
+     */
+
+
+    /**
+     * 이미지 이름과 단건 조회
+     */
+    
+    
+    /**
+     * 이미지 날짜와 단건 조회
+     */
 }
