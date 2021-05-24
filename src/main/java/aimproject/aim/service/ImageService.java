7347bb4636c650c5e596003bbc299d9b3a70faeb
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-25 03:58:21 +0900

    Add service function method for set imageName with UUID

--- src/main/java/aimproject/aim/service/ImageService.java
+++ src/main/java/aimproject/aim/service/ImageService.java
@@ -2,11 +2,13 @@ package aimproject.aim.service;
 
 import aimproject.aim.model.Image;
 import aimproject.aim.repository.ImageRepository;
+import aimproject.aim.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +16,7 @@ import java.util.List;
 public class ImageService {
 
     private final ImageRepository imageRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 이미지 정보 저장
@@ -23,6 +26,17 @@ public class ImageService {
 
 
         imageRepository.save(image);
+    }
+
+    /**
+     * 이미지 이름 세팅 부분
+     */
+    public String setImageNameByUUID(String originalName, String memberId) throws Exception {
+
+        UUID uid = UUID.randomUUID();   // UUID 랜덤 자동 생성 부문
+        String imageName = uid + "_" + memberId + "_" + originalName;
+
+        return imageName;
     }
 
     /**
