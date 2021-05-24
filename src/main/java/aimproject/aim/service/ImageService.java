9f5ca4fb2fe4e539ad103d81fd3e55ba1b599e61
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-24 23:43:40 +0900

    Update ImageService to add service methods for service function

--- src/main/java/aimproject/aim/service/ImageService.java
+++ src/main/java/aimproject/aim/service/ImageService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -18,30 +20,37 @@ public class ImageService {
      */
     @Transactional
     public void save(Image image) {
+
+
         imageRepository.save(image);
     }
 
     /**
      * 이미지 전체 조회
      */
-
+    public List<Image> findAll() {
+        return imageRepository.findAll();
+    }
 
     /**
      * 이미지 건별 조회
      */
-
+    public Image findOne(Long imageId) {
+        return imageRepository.findOne(imageId);
+    }
 
     /**
      * 이미지 아이디와 단건 조회
      */
-
+    public Image findByImageId(String memberId, Long imageId) {
+        return imageRepository.findByImageId(memberId, imageId);
+    }
 
     /**
      * 이미지 이름과 단건 조회
      */
-    
-    
-    /**
-     * 이미지 날짜와 단건 조회
-     */
+    public Image findByImageName(String memberId, String imageName) {
+        return imageRepository.findByImageName(memberId, imageName);
+    }
+
 }
