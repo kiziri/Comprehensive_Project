1ee225ae306ba83c8c5b2d61ccfafad6bb7afeb4
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-29 02:21:30 +0900

    Update ImageController, image analysis handler

--- src/main/java/aimproject/aim/controller/ImageController.java
+++ src/main/java/aimproject/aim/controller/ImageController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
 
 @Controller
 @RequiredArgsConstructor
@@ -44,33 +43,23 @@ public class ImageController {
         log.info("imageOriginName: " + file.getOriginalFilename());
         log.info("size: " + file.getSize());
         log.info("contentType: " + file.getContentType());
+        log.info("contentLength: " + file.getResource());
 
-        // UUID 설정된 이미지파일명 설정
-        String imageNameByUUID = imageService.setImageNameByUUID(file.getOriginalFilename(), memberId);
-
-        Image image = new Image();
-        image.setImageName(imageNameByUUID);
-        image.setImageOriginName(file.getOriginalFilename());
-        image.setImageDate(LocalDateTime.now());
-
-        Long imageId = imageService.save(memberId, image);
+        Long imageId = imageService.save(memberId, file);
         log.info(""+imageId);
+        Image image = imageService.findOne(imageId);
 
         // 해당 이미지명 및 경로 모델로 전송
-        model.addAttribute("savedName", imageNameByUUID);
-
-        // 파일 저장은 서비스단에서 구현
-        //File target = new File(directoryPath, imageNameByUUID);  // 파일명과 경로 지정
-        //FileCopyUtils.copy(file.getBytes(), target);    // target에 해당하는 파일 생성
+        model.addAttribute("imageName", image.getImageName());
+        model.addAttribute("image", image);
+        model.addAttribute("memberNickname", member.getNickname());
 
         return "redirect:/result/{memberNickname}";
     }
-
-
-
+    
     //프론트 이미지 출력
     @GetMapping(value = "/result/{memberNickname}")
-    public void loadImage(Model model, HttpServletRequest request, @PathVariable String memberNickname) {
+    public void loadImage(@PathVariable String memberNickname, Model model, HttpServletRequest request) {
         
     }
 }
