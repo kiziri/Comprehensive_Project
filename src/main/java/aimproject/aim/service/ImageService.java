2e6b5277e986c9c5b38852772b25216a398fbd32
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-29 02:22:16 +0900

    Update ImageService for image save to server and database

--- src/main/java/aimproject/aim/service/ImageService.java
+++ src/main/java/aimproject/aim/service/ImageService.java
@@ -5,9 +5,15 @@ import aimproject.aim.model.Member;
 import aimproject.aim.repository.ImageRepository;
 import aimproject.aim.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,17 +24,34 @@ public class ImageService {
 
     private final ImageRepository imageRepository;
     private final MemberRepository memberRepository;
+    
+    @Value("${file.upload.directory}")
+    private static String imagePath;
 
     /**
      * 이미지 정보 저장
      */
     @Transactional
-    public Long save(String memberId, Image imageInfo) {
+    public Long save(String memberId, MultipartFile file) throws IOException {
 
         // 회원 정보 조회
         Member member = memberRepository.findOne(memberId);
 
+        Image imageInfo = new Image();
+        String imageName = setImageNameByUUID(file.getOriginalFilename(), memberId);
+        String imageResourcePath = imagePath + "/" +imageName;
+
+        imageInfo.setMember(member);
+        imageInfo.setImageName(imageName);
+        imageInfo.setImageOriginName(file.getOriginalFilename());
+        imageInfo.setImagePath(imageResourcePath);
+        imageInfo.setImageDate(LocalDateTime.now());
+
         Image image = Image.createImage(imageInfo, member);
+
+        // 파일 저장은 서비스단에서 구현
+        File target = new File(imagePath, imageName);  // 파일명과 경로 지정
+        FileCopyUtils.copy(file.getBytes(), target);    // target에 해당하는 파일 생성
 
         imageRepository.save(image);
         return image.getImageId();
@@ -37,7 +60,7 @@ public class ImageService {
     /**
      * 이미지 이름 세팅 부분
      */
-    public String setImageNameByUUID(String originalName, String memberId) throws Exception {
+    public String setImageNameByUUID(String originalName, String memberId) {
 
         UUID uid = UUID.randomUUID();   // UUID 랜덤 자동 생성 부문
         String imageName = uid + "_" + memberId + "_" + originalName;
