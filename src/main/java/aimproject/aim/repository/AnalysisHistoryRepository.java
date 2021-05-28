4ce4ad870f91f6866044fc2c698387844bf2fd0c
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-29 02:32:02 +0900

    Add new file for mypage functions

---
+++ src/main/java/aimproject/aim/repository/AnalysisHistoryRepository.java
@@ -0,0 +1,11 @@
+package aimproject.aim.repository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+@Slf4j
+public class AnalysisHistoryRepository {
+}
