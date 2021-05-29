c9b54fe25a47d405306047b3b7e6c6ba18aa6a36
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-29 16:09:40 +0900

    Update to optimize import

--- src/main/java/aimproject/aim/repository/ImageRepository.java
+++ src/main/java/aimproject/aim/repository/ImageRepository.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
