38bc03694ac983aecae39b6cb8cef09a6c256d7c
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-31 16:16:22 +0900

    Update test file for retetst

--- src/test/java/aimproject/aim/service/MultipartTest.java
+++ src/test/java/aimproject/aim/service/MultipartTest.java
@@ -1,4 +1,4 @@
-package aimproject.aim;
+package aimproject.aim.service;
 
 
 import org.json.simple.JSONArray;
@@ -25,7 +25,8 @@ public class MultipartTest {
     private OutputStream outputStream;
     private PrintWriter writer;
 
-    public void addTextPart(String name, String value) throws IOException {// iput text 추가 함수 삭제 해도 됨
+    // iput text 추가 함수 삭제 해도 됨
+    public void addTextPart(String name, String value) throws IOException {
         writer.append("--" + boundary).append(LINE_FEED);
         writer.append("Content-Disposition: form-data; name=\"" + name + "\"").append(LINE_FEED);
         writer.append("Content-Type: text/plain; charset=" + charset).append(LINE_FEED);
@@ -34,7 +35,9 @@ public class MultipartTest {
         writer.flush();
     }
 
-    public void addFilePart(String name, File file) throws IOException {//input file 추가 함수 필수
+    //input file 추가 함수 필수
+    public void addFilePart(String name, File file) throws IOException {
+        // 이미지 데이터를 바디의 폼 데이터 형식으로 변환
         writer.append("--" + boundary).append(LINE_FEED);
         writer.append("Content-Disposition: form-data; name=\"" + name + "\"; filename=\"" + file.getName() + "\"").append(LINE_FEED);
         writer.append("Content-Type: " + URLConnection.guessContentTypeFromName(file.getName())).append(LINE_FEED);
@@ -42,6 +45,7 @@ public class MultipartTest {
         writer.append(LINE_FEED);
         writer.flush();
 
+        // 변환한 형식을 바이트화하여 전송
         FileInputStream inputStream = new FileInputStream(file);
         byte[] buffer = new byte[(int) file.length()];
         int bytesRead = -1;
@@ -54,12 +58,12 @@ public class MultipartTest {
         writer.flush();
     }
 
-
-    public void sendMultiPart() {// 메인 함수 전송 반환 모든 부분을 담당
+    // 메인 함수 전송 반환 모든 부분을 담당
+    public void sendMultiPart() {
         try {
             File file = new File("src/main/resources/static/imageupload/face.jpg");// 보낼 파일 설정
 
-            URL url = new URL("http://localhost:3000/up");//url 경로 설정
+            URL url = new URL("http://localhost:3000/analysis");//url 경로 설정
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();// 연결 생성
 
             //http 해더 설정
@@ -73,50 +77,37 @@ public class MultipartTest {
             outputStream = connection.getOutputStream();
             writer = new PrintWriter(new OutputStreamWriter(outputStream, charset), true);
 
-
             //body 데이터 세팅
             addFilePart("img", file);
             writer.append("--" + boundary + "--").append(LINE_FEED);
             writer.close();
 
-
             String responseData = "";
             String returnData = "";
 
             //http 요청 후 응답 받은 데이터를 버퍼에 쌓는다
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-            StringBuffer sb = new StringBuffer();
-            while ((responseData = br.readLine()) != null) {
-                sb.append(responseData); //StringBuffer에 응답받은 데이터 순차적으로 저장 실시
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+            StringBuffer stringBuffer = new StringBuffer();
+            while ((responseData = bufferedReader.readLine()) != null) {
+                stringBuffer.append(responseData); //StringBuffer에 응답받은 데이터 순차적으로 저장 실시
             }
 
             //응답 받은 데이터 출력
-            returnData = sb.toString();
-
+            returnData = stringBuffer.toString();
             System.out.println(returnData);
 
-            //오브젝트 화
+            //오브젝트화
             JSONParser parser = new JSONParser();
             JSONObject object = (JSONObject) parser.parse(returnData);
             JSONArray arr= (JSONArray)object.get("data");
 
             //오브젝트 내부에서 볼러오기
             JSONObject temp = (JSONObject) arr.get(0);
-            //int face = (int) temp.get("face");
             System.out.println(temp.toString());
             System.out.println(temp.get("sad").toString());
-
-
-
-
-
-
-
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
 }
