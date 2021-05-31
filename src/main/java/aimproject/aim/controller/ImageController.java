2c2c1c08c7cc2e3b4ff3afd388d9f326d4878b2c
kiziri <58429073+kiziri@users.noreply.github.com>
2021-05-31 23:58:05 +0900

    Delete log.info code in handler update result page handler update image analysis request function

--- src/main/java/aimproject/aim/controller/ImageController.java
+++ src/main/java/aimproject/aim/controller/ImageController.java
@@ -5,6 +5,8 @@ import aimproject.aim.model.Member;
 import aimproject.aim.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.buf.Utf8Encoder;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,11 +36,6 @@ public class ImageController {
         return "page/demo_page";
     }
 
-    /*@RequestMapping("/result")
-    public String result(Model model) {
-        return "page/result_page";
-    }*/
-
     @PostMapping("/demo/analysis")
     public String imageAnalysis(MultipartFile file, RedirectAttributes redirectAttributes, HttpServletRequest request) throws Exception {
         // 세션 받아오기
@@ -47,17 +44,10 @@ public class ImageController {
         // 요청한 세션의 정보를 가져와 회원의 아이디 저장
         Member member = (Member)session.getAttribute("member");
         String memberId = member.getMemberId();
-        log.info(memberId);
-
-        // 파일 속성 출력
-        log.info("imageOriginName: " + file.getOriginalFilename());
-        log.info("size: " + file.getSize());
-        log.info("contentType: " + file.getContentType());
-        log.info("contentLength: " + file.getResource());
 
         Long imageId = imageService.save(memberId, file);
-        log.info(""+imageId);
         Image image = imageService.findOne(imageId);
+        log.info(image.getImagePath());
 
         // 세션에 이미지 객체 정보 저장
         session.setAttribute("image", image);
@@ -69,36 +59,23 @@ public class ImageController {
     
     //프론트 이미지 출력
     @GetMapping(value = "/result/{memberId}")
-    public String loadImage(@PathVariable String memberId, Model model, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    public String loadImage(@PathVariable String memberId, Model model, HttpServletRequest request) {
         // 세션 받아오기
-        HttpSession session = request.getSession(false);
+        HttpSession session = request.getSession();
 
-        // 회원 및 잉미지 정보를 가져오기
-        Member member = (Member) session.getAttribute("member");
-        Image image = (Image) session.getAttribute("image");
+        // 세션 이미지 정보 및 DB 이미지 정보 가져오기
+        Image imageSession = (Image) session.getAttribute("image");
+        String imagePath = imageSession.getImagePath();
+
+        // 이미지 분석 요청
+        Image image = imageService.findByImageName(memberId, imageSession.getImageName());
+        JSONObject resultJSON = imageService.requestForAnalysis(image);
 
         // 세션으로부터 받은 객체로 불러올 경로를 설정
-
-        model.addAttribute("imagePath", image.getImagePath());
+        model.addAttribute("imagePath", imagePath);
 
         return "page/result_page";
     }
 
-    @GetMapping(value = "/imageUpload/{imagename}")
-    public void LoadImage(@PathVariable("imagename") String imagename, HttpServletResponse response) {
-        File image = new File("C:\\Users\\yunhc\\Desktop\\Aim_Project\\imageUpload\\" + imagename);
-        int cur;
-        try {
-            FileInputStream fileIn = new FileInputStream(image);
-            BufferedInputStream bufIn = new BufferedInputStream(fileIn);
-            ServletOutputStream ostream = response.getOutputStream();
-            while ((cur = bufIn.read()) != -1) {
-                ostream.write(cur);
-            }
-            ostream.flush();
-            bufIn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+
 }
