1f561f3d0bfba9326e3dc6b3b57d24ee9af8eea7
2015244007_yun <46712186+yunhc99@users.noreply.github.com>
2021-06-02 00:50:11 +0900

    Update result_script

--- src/main/resources/static/javascript/result_page_js/result_script.js
+++ src/main/resources/static/javascript/result_page_js/result_script.js
@@ -1,60 +1,6 @@
 console.log("js result");
 
 
-function prettyJson(jsonText) {
-    if (!jsonText) {
-        return jsonText;
-    }
-    var prettyJson = new Array();
-    var depth = 0;
-    var currChar;
-    var prevChar;
-    var doubleQuoteIn = false;
-    for (var i = 0; i < jsonText.length; i++) {
-        currChar = jsonText.charAt(i);
-        if (currChar == '\"') {
-            if (prevChar != '\\') {
-                doubleQuoteIn = !doubleQuoteIn;
-            }
-        }
-        switch (currChar) {
-            case '{':
-                prettyJson.push(currChar);
-                if (!doubleQuoteIn) {
-                    prettyJson.push('\n');
-                    insertTab(prettyJson, ++depth);
-                }
-                break;
-            case '}':
-                if (!doubleQuoteIn) {
-                    prettyJson.push('\n');
-                    insertTab(prettyJson, --depth);
-                }
-                prettyJson.push(currChar);
-                break;
-            case ',':
-                prettyJson.push(currChar);
-                if (!doubleQuoteIn) {
-                    prettyJson.push('\n');
-                    insertTab(prettyJson, depth);
-                }
-                break;
-            default:
-                prettyJson.push(currChar);
-                break;
-        }
-        prevChar = currChar;
-    }
-    return prettyJson.join('');
-}
-
-
-function insertTab(prettyJson, depth) {
-    for (var i = 0; i < depth; i++) {
-        prettyJson.push(TAB);
-    }
-}
-
 
 
 
